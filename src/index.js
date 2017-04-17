6bca9c5d0cba22c882a878eb4dd436009349d6e6
Alberto Miranda <codealchemist@gmail.com>
2017-04-17 01:46:43 -0300

    Sending document changes.

--- src/index.js
+++ src/index.js
@@ -4,19 +4,29 @@ const uuid = require('uuid/v4')
 
 console.log(`SOCKET.IO SERVER listening on port ${port}`)
 
-// TODO: create rooms for each document
 io.on('connection', (socket) => {
   console.log('-- socket connected')
   socket.emit('connected')
 
   socket.on('create', () => {
     const id = uuid()
-    console.log('--- new document, id: ', id)
+    console.log('--- new document, id:', id)
+    socket.join(id)
     socket.emit('created', id)
   })
 
   socket.on('join', (id) => {
-    console.log('--- join document room: ', id)
+    console.log('--- join document:', id)
+    socket.join(id)
     socket.emit('joined', id)
+
+    const {remoteAddress, remotePort} = socket.request.connection
+    console.log(`--- client address:`, {remoteAddress, remotePort})
+    socket.broadcast.to(id).emit('newPeer', {remoteAddress, remotePort})
+  })
+
+  socket.on('documentChange', ({id, data}) => {
+    console.log(`--- document ${id} changed:`, data)
+    socket.broadcast.to(id).emit('documentChanged', {id, data})
   })
 })
